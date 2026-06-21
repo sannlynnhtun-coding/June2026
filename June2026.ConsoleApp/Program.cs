f20ad0471f9af96a46dcbffa065c315ae4922f67
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-06-21 20:56:49 +0630

    feat: test hello world

--- June2026.ConsoleApp/Program.cs
+++ June2026.ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//Console.ReadKey();
+string message = Console.ReadLine();
+Console.WriteLine("hello, " + message);
+
+Console.ReadLine();
