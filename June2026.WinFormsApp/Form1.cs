987086d10593a415e2e91a1fff26da25dfd78b35
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-06-28 02:27:33 +0630

    feat: add windows forms app

---
+++ June2026.WinFormsApp/Form1.cs
@@ -0,0 +1,15 @@
+namespace June2026.WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+C# => Database
+
+
+ADO.NET 
+Dapper
+EFCore
+
+nuget package
+npm
+pub.dev
+
+Tab
+
+Ctrl + .
+
+Ctrl + Shift + F5
+
+Query - R / Execute - CUD
