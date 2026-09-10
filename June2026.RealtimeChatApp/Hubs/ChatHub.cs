c4dcf2f2098faaf861f82d1cb3bb0c423509ce1c
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-09-10 23:00:23 +0630

    feat: ASP.NET Core MVC + SignalR

---
+++ June2026.RealtimeChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace June2026.RealtimeChatApp.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task ServerReceiveMessageEvent(string user, string message)
+    {
+        await Clients.All.SendAsync("ClientReceiveMessageEvent", user, message);
+    }
+}
