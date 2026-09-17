1aecb6259b0bd91a3aeb051e384034348238750a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-09-17 09:23:24 +0630

    feat: add Middleware

---
+++ June2026.MiddlewareWebApp/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace June2026.MiddlewareWebApp.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(LoginRequestModel requestModel)
+    {
+        Response.Cookies.Append("username", requestModel.username);
+        return Redirect("/home");
+    }
+}
+
+public class LoginRequestModel
+{
+    public string username { get; set; }
+    public string password { get; set; }
+}
---
+++ June2026.MiddlewareWebApp/Middlewares/CookieMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace June2026.MiddlewareWebApp.Middlewares;
+
+public class CookieMiddleware
+{
+    private readonly ILogger<CookieMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public CookieMiddleware(RequestDelegate next, ILogger<CookieMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        _logger.LogInformation("CookieMiddleware => {path}", context.Request.Path);
+        if (context.Request.Path.ToString().ToLower() != "/login" &&
+            context.Request.Path.ToString().ToLower() != "/login/index")
+        {
+            if (!context.Request.Cookies.Any(x => x.Key == "username"))
+            {
+                context.Response.Redirect("/login");
+            }
+        }
+
+        // Call the next delegate/middleware in the pipeline.
+        await _next(context);
+    }
+}
+
+public static class CookieMiddlewareExtension
+{
+    public static void UseCookieMiddleware(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<CookieMiddleware>();
+    }
+}
